76c27f878817f1b2dbb1c6307a045a5a52153c12
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2017-10-30 18:02:04 +0800

    Add files via upload

---
+++ my-project-master/src/ECHO/com/Person.java
@@ -0,0 +1,14 @@
+package ECHO.com;
+                                                                     
+public class Person 
+{
+public void sayHello()
+   {
+	System.out.println("Hello");
+   }
+public static void main(String[] args) 
+{
+ Person p = new Person();
+ p.sayHello();
+}
+}

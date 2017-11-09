f517a9184225dddf917760791a2c0ccf3c153c45
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2017-11-09 23:45:50 +0800

    Add files via upload

---
+++ Ex1.com/src/com/joe/Triangle.java
@@ -0,0 +1,26 @@
+package com.joe;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	for(int j = 1;j<=5;j++)//執行次數
+	{for(int k = 4;k>=j;k--)//遞增"<=",地減">="
+		System.out.print(" ");
+	for(int i = 1;i<=2*j-1;i++)
+		System.out.print("*");
+		System.out.println();
+	}
+		
+		
+		
+		
+			
+		}
+			
+			
+			
+		}
+		
+	
+

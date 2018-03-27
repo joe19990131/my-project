0db105f77fdcd1953fc704ada46b9b431a8855a6
joe199990131 <joe21854946@gmail.com>
2018-03-27 16:30:00 +0800

    0327

--- src/com/java2/object/Texter.java
+++ src/com/java2/object/Texter.java
@@ -1,5 +1,6 @@
 package com.java2.object;
 import java.util.*;
+import java.util.List;
 public class Texter {
 
 	public static void main(String[] args) {
@@ -8,9 +9,13 @@ public class Texter {
 		Boolean a = new Boolean(true);
 		System.out.println("Please enter patient's name ");
 		
-		
-		
-		
+		List<String> name = new ArrayList<>();
+		name.add("john");
+		name.add("amy");
+		name.add("alice");
+		System.out.println("removing name:"+name.remove(0));
+		System.out.println(name.size());
+		System.out.println(name.get(1));
 		while(a){
 			Scanner sc1 = new Scanner(System.in);
 			String a1 = sc1.nextLine();

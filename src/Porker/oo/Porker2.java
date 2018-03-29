cba5665e295ffbda6e0f63cb4568d65a6f5bf1a3
joe19990131 <joe21854946@gmail.com>
2018-03-30 02:17:21 +0800

    new porker

--- src/Porker/oo/Porker2.java
+++ src/Porker/oo/Porker2.java
@@ -2,34 +2,88 @@ package Porker.oo;
 
 import java.util.Random;
 import java.util.*;
+
 public class Porker2 {
 	Random r1 = new Random();
 	String flower = "SHDC";
 	List<Integer> card = new ArrayList<>();
-	//card check
+
+	// card check
 	public void porker() {
-		for(int i = 0;i<52;i++){
+		for (int i = 0; i < 52; i++) {
 			card.add(i);
-			/*System.out.print((i % 13) + 1 + "" + (flower.charAt(i / 13))+"\t");
-			if((i+1)%13==0) {
-				System.out.println();
-			}*/
+			/*
+			 * System.out.print((i % 13) + 1 + "" + (flower.charAt(i / 13))+"\t");
+			 * if((i+1)%13==0) { System.out.println(); }
+			 */
 		}
 	}
+
 	public void shuffle() {
-		for(int i = 0;i<card.size();i++) {
+		for (int i = 0; i < card.size(); i++) {
 			int r = r1.nextInt(51);
 			int tmp = card.get(i);
 			card.set(i, card.get(r));
 			card.set(r, tmp);
 		}
 	}
+
 	public void show() {
-		for(int i = 0;i<card.size();i++) {
+		for (int i = 0; i < card.size(); i++) {
 			int c = card.get(i);
-			System.out.print((c%13)+1+""+(flower.charAt(c/13))+"\t");
-			if((i+1)%13==0) {
+			if ((c + 13 + 1) % 13 == 1) {
+				System.out.print("A" + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((c + 13 + 1) % 13 > 1 && (c + 13 + 1) % 13 < 11) {
+				System.out.print((c % 13) + 1 + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((c + 13 + 1) % 13 == 11) {
+				System.out.print("J" + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((c + 13 + 1) % 13 == 12) {
+				System.out.print("Q" + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((c + 13 + 1) % 13 == 0) {
+				System.out.print("K" + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((i + 1) % 13 == 0) {
 				System.out.println();
+			}
+		}
+	}
+
+	public void deal() {
+		for (int i = 0; i < 13; i++) {
+			int c = card.get(i);
+			if ((c + 13 + 1) % 13 == 1) {
+				System.out.print("A" + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((c + 13 + 1) % 13 > 1 && (c + 13 + 1) % 13 < 11) {
+				System.out.print((c % 13) + 1 + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((c + 13 + 1) % 13 == 11) {
+				System.out.print("J" + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((c + 13 + 1) % 13 == 12) {
+				System.out.print("Q" + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((c + 13 + 1) % 13 == 0) {
+				System.out.print("K" + "" + (flower.charAt(c / 13)) + "\t");
+			}
+			if ((i + 1) % 13 == 0) {
+				System.out.println();
+			}
+		}
+	}
+
+	public void sort() {
+		for (int i = 0; i < card.size(); i++) {
+			for (int j = i + 1; j < card.size(); j++) {
+				if (card.get(i) > card.get(j)) {
+					int tmp = card.get(i);
+					card.set(i, card.get(j));
+					card.set(j, tmp);
+				}
 			}
 		}
 	}
--- src/Porker/oo/PorkerTester2.java
+++ src/Porker/oo/PorkerTester2.java
@@ -1,5 +1,5 @@
 package Porker.oo;
-
+import java.util.*;
 public class PorkerTester2 {
 
 	public static void main(String[] args) {
@@ -7,6 +7,17 @@ public class PorkerTester2 {
 		Porker2 p2 = new Porker2();
 		p2.porker();
 		p2.shuffle();
+		p2.show();
+		//p2.deal();
+		Scanner sc1 = new Scanner(System.in);
+		String q = sc1.nextLine();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		p2.sort();
 		p2.show();
 	}
 

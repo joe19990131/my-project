c7b470c1bb54816bed7dd1f228df341ab6be5746
joe19990131 <joe21854946@gmail.com>
2018-04-10 16:26:41 +0800

    javaIO

---
+++ src/com/file/Filetest.java
@@ -0,0 +1,29 @@
+package com.file;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Filetest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileOutputStream out = new FileOutputStream("data.txt");
+			out.write(65);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("File Not Found!!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Err!!");
+
+		}
+	}
+
+}

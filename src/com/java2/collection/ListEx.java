7345c2aaf7e454364af7dd8132602e8379588426
joe19990131 <joe21854946@gmail.com>
2018-05-20 22:14:01 +0800

    sstepik upgrade

--- src/com/java2/collection/ListEx.java
+++ src/com/java2/collection/ListEx.java
@@ -6,14 +6,25 @@ public class ListEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Set<String> nameSet = new TreeSet<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
-		String[] namelist = nameSet.toArray(new String[0]);
-		System.out.println(namelist[0]);
-		System.out.println(namelist[1]);
-		System.out.println(namelist[2]);
-		
+		List<Integer> list = new ArrayList<>();
+		for (int i = 1; i <= 7; i++) {
+			list.add(i);
+		}
 
-	    
+		list.remove(6);
+		list.remove(4);
+		list.remove(2);
+		list.remove(0);
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = i + 1; j < list.size(); j++)
+
+				if (list.get(i) < list.get(j)) {
+					int tmp = list.get(i);
+					list.set(i, list.get(j));
+					list.set(j, tmp);
+				}
+		}
+		System.out.print(list.get(0)+" "+list.get(1)+" "+list.get(2));
 	}
 
 }

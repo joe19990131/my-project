7d89be84edc7b573a35469a8199769c1ccb054de
joe19990131 <joe21854946@gmail.com>
2018-06-05 18:11:33 +0800

    vender v2

--- src/com/java2/oo/VenderMachineMain.java
+++ src/com/java2/oo/VenderMachineMain.java
@@ -1,11 +1,40 @@
 package com.java2.oo;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class VenderMachineMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Vendingmachine vm = new Vendingmachine();
+		try {
+			FileInputStream fis = new FileInputStream("drinks.txt");
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader in = new BufferedReader(isr);
+			String line = in.readLine();
+			while(line!=null){
+				String[] token = line.split(",");
+				String[] tokens = line.split(",");
+				int count = Integer.parseInt(tokens[0]);
+				for (int i = 0; i < count; i++){
+					String name = tokens[i*2+1];
+					int price = Integer.parseInt(tokens[i*2+2]);
+					vm.drinks.add(new Drink(i+1, name, price));
+				}
+				vm.on();
 		
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
--- src/com/java2/oo/Vendingmachine.java
+++ src/com/java2/oo/Vendingmachine.java
@@ -20,36 +20,7 @@ public class Vendingmachine {
 	public void addDrink(int id,String name,int price){
 		drinks.add(new Drink(id, name, price));
 	}
-	public void reader(){
-		try {
-			FileInputStream fis = new FileInputStream("drinks.txt");
-			InputStreamReader isr = new InputStreamReader(fis);
-			BufferedReader in = new BufferedReader(isr);
-			String line = in.readLine();
-			while(line!=null){
-				String[] token = line.split(",");
-				try {
-					int id = Integer.parseInt(token[0]);
-					String name = token[1];
-					int price = Integer.parseInt(token[2]);
-					Drink dr = new Drink(id, name, price);
-					drinks.add(dr);
-				} catch (NumberFormatException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					System.out.println("wrong data structure!!");
-				}
-				line = in.readLine();
-		
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	
 	public void on(){
 		for(int i = 0;i<drinks.size();i++){
 			drinks.get(i);

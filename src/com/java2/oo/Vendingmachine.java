d410e6848a1e03a71e3b2c7e8f3235cd55df97b5
joe19990131 <joe21854946@gmail.com>
2018-06-07 14:51:13 +0800

    vender2

--- src/com/java2/oo/Vendingmachine.java
+++ src/com/java2/oo/Vendingmachine.java
@@ -13,21 +13,63 @@ import java.util.Scanner;
 
 public class Vendingmachine {
 	List<Drink> drinks = new ArrayList<>();
-	public Vendingmachine(){
-		
+
+	public Vendingmachine() {
+
 	}
-	
-	public void addDrink(int id,String name,int price){
+
+	public void addDrink(int id, String name, int price) {
 		drinks.add(new Drink(id, name, price));
 	}
-	
-	public void on(){
+
+	public void on() {
 		Scanner sc1 = new Scanner(System.in);
-		for(int i = 0;i<drinks.size();i++){
-			drinks.get(i);
-			
-			
+		Scanner sc2 = new Scanner(System.in);
+		int sw = -1;
+		int sw1 = -1;
+		int coin = 0;
+		for (int i = 0; i < drinks.size(); i++) {
+			Drink drink = drinks.get(i);
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+
 		}
-		System.out.println("請選擇飲料");
+		System.out.println("請投幣或按0結束投幣");
+		
+			while (sw1 != 0) {
+				switch (sc1.nextInt()) {
+				case 1:
+					coin++;
+					break;
+				case 5:
+					coin = coin + 5;
+					break;
+				case 10:
+					coin = coin + 10;
+					break;
+				case 50:
+					coin = coin + 50;
+					break;
+				case 0:
+					sw1 = 0;
+					break;
+				}
+			}
+			
+			while (sw != 0) {
+			switch(sc2.nextInt()) {
+			case 1:
+				coin = coin-25;
+				break;
+			case 2:
+				coin = coin-30;
+				break;
+			case 3:
+				coin = coin-20;
+				break;
+			case 0:
+				sw = 0;
+				break;
+			}
+		}
 	}
 }
